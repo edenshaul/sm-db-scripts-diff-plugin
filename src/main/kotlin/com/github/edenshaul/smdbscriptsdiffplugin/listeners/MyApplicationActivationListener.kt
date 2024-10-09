1830bf347b63586cdf0b5b35862bbe37f08453cd
GitHub Action <action@github.com>
2024-10-09 19:20:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/edenshaul/smdbscriptsdiffplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/edenshaul/smdbscriptsdiffplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.edenshaul.smdbscriptsdiffplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
